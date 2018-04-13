7ad7b294cc32988ec6192b97014fa7ab38c76de3
Arsh Singh <33452790+iosdev474@users.noreply.github.com>
2018-04-13 13:00:20 +0530

    Add source code
    Added source code of CSS aimbot.

---
+++ main.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace CSS_Aimbot_iOSDev
+{
+    partial class Aimbot_iOSDev
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Aimbot_iOSDev));
+            this.injectButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.previousButton = new System.Windows.Forms.Button();
+            this.nextButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(140, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(132, 106);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Enemy Info:";
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(122, 106);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Player Info:";
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Arial Black", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Lime;
+            this.label3.Location = new System.Drawing.Point(12, 227);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(139, 22);
+            this.label3.TabIndex = 1;
+            // 
+            // previousButton
+            // 
+            this.previousButton.BackColor = System.Drawing.Color.Gainsboro;
+            this.previousButton.Location = new System.Drawing.Point(10, 140);
+            this.previousButton.Name = "previousButton";
+            this.previousButton.Size = new System.Drawing.Size(130, 60);
+            this.previousButton.TabIndex = 0;
+            this.previousButton.Text = "Previous";
+            this.previousButton.UseVisualStyleBackColor = false;
+            this.previousButton.Click += new System.EventHandler(this.previousButton_Click);
+            // 
+            // nextButton
+            // 
+            this.nextButton.BackColor = System.Drawing.Color.Gainsboro;
+            this.nextButton.Location = new System.Drawing.Point(140, 140);
+            this.nextButton.Name = "nextButton";
+            this.nextButton.Size = new System.Drawing.Size(130, 60);
+            this.nextButton.TabIndex = 0;
+            this.nextButton.Text = "Next";
+            this.nextButton.UseVisualStyleBackColor = false;
+            this.nextButton.Click += new System.EventHandler(this.nextButton_Click);
+            // 
+            // injectButton
+            // 
+            this.injectButton.BackColor = System.Drawing.Color.Gainsboro;
+            this.injectButton.Location = new System.Drawing.Point(155, 209);
+            this.injectButton.Name = "injectButton";
+            this.injectButton.Size = new System.Drawing.Size(115, 40);
+            this.injectButton.TabIndex = 0;
+            this.injectButton.Text = "Inject";
+            this.injectButton.UseVisualStyleBackColor = false;
+            this.injectButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Aimbot_iOSDev
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImage = global::CSS_Aimbot_iOSDev.Properties.Resources.back;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.nextButton);
+            this.Controls.Add(this.previousButton);
+            this.Controls.Add(this.injectButton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Aimbot_iOSDev";
+            this.Text = "Aimbot by iOSDev";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.Label label2;
+        public System.Windows.Forms.Label label1;
+        public System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button previousButton;
+        private System.Windows.Forms.Button nextButton;
+        private System.Windows.Forms.Button injectButton;
+    }
+}
+
---
+++ main.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSS_Aimbot_iOSDev
+{
+
+    public partial class Aimbot_iOSDev : Form
+    {
+        int toggle = 1;
+        [DllImport("kernel32.dll")]
+        private static extern IntPtr OpenProcess(ProcessAccessFlags dwDesiredAccess, [MarshalAs(UnmanagedType.Bool)] bool bInheritHandle, int dwProcessId);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern bool WriteProcessMemory(IntPtr hProcess, int lpBaseAddress, byte[] lpBuffer, uint nSize, out int lpNumberOfBytesWritten);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern bool ReadProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, [Out] byte[] lpBuffer, int dwSize, out int lpNumberOfBytesRead);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern bool CloseHandle(IntPtr hObject);
+
+        [Flags]
+        public enum ProcessAccessFlags : uint
+        {
+            All = 0x001F0FFF,
+            Terminate = 0x00000001,
+            CreateThread = 0x00000002,
+            VMOperation = 0x00000008,
+            VMRead = 0x00000010,
+            VMWrite = 0x00000020,
+            DupHandle = 0x00000040,
+            SetInformation = 0x00000200,
+            QueryInformation = 0x00000400,
+            Synchronize = 0x00100000
+        }
+
+        int serverModuleAddress = 0, engineModuleAddress = 0;
+        int playerBaseAddress = 0x54AB70;
+        int enemyBaseAddress = 0x54AB70 + 0x24;
+        int angleYAddress = 0x4632D4;
+        int angleXAddress = 0x4632D8;
+
+        int getModuleAddress(string name)
+        {
+            Process process = Process.GetProcessesByName("hl2")[0];
+            foreach (ProcessModule module in process.Modules)
+            {
+                if (module.ModuleName == name)
+                {
+                    return (int)module.BaseAddress;
+                }
+            }
+            return 0;
+        }
+
+        float getEntityPos(int i, bool isPlayer)
+        {
+            int totalAddress = serverModuleAddress + (isPlayer ? playerBaseAddress : enemyBaseAddress);
+            Process process = Process.GetProcessesByName("hl2")[0];
+            IntPtr pHandle = OpenProcess(ProcessAccessFlags.All, true, process.Id);
+            byte[] results = new byte[4];
+            int read = 0;
+            if (ReadProcessMemory(pHandle, (IntPtr)totalAddress + (4 * i), results, results.Length, out read))
+            {
+                CloseHandle(pHandle);
+                return BitConverter.ToSingle(results, 0);
+            }
+            CloseHandle(pHandle);
+            return 0;
+        }
+
+        void changeAngle(float a, bool isX_Angle)
+        {
+            int totalAddress = engineModuleAddress + (isX_Angle ? angleXAddress : angleYAddress);
+            Process process = Process.GetProcessesByName("hl2")[0];
+            IntPtr pHandle = OpenProcess(ProcessAccessFlags.All, true, process.Id);
+            int written = 0;
+            WriteProcessMemory(pHandle, totalAddress, BitConverter.GetBytes(a), 4, out written);
+            CloseHandle(pHandle);
+        }
+
+        bool started = false;
+        async void update()
+        {
+            while (true)
+            {
+                if (started)
+                {
+                    float x1 = getEntityPos(0, true);
+                    float y1 = getEntityPos(1, true);
+                    float z1 = getEntityPos(2, true);
+
+                    float x2 = getEntityPos(0, false);
+                    float y2 = getEntityPos(1, false);
+                    float z2 = getEntityPos(2, false) - 10;
+
+                    double distance_X = x2 - x1;
+                    double distance_Y = y2 - y1;
+                    double distance_Z = z2 - z1;
+                    double distance_XY_Plane = Math.Sqrt(Math.Pow(distance_X, 2) + Math.Pow(distance_Y, 2));
+                    label1.Text = "Player Info: \nX : " + x1 + "\nY : " + y1 + "\nZ : " + z1;
+                    label2.Text = "Enemy Info: \nX : " + x2 + "\nY : " + y2 + "\nZ : " + z2;
+                    double x_r = Math.Acos(distance_X / distance_XY_Plane) * 180 / Math.PI;
+                    x_r *= (y2 < y1) ? -1 : 1;
+                    changeAngle((float)x_r, true);
+
+                    double y_r = -1 * Math.Atan(distance_Z / distance_XY_Plane) * 180 / Math.PI;
+                    changeAngle((float)y_r, false);
+                }
+                await Task.Delay(1);
+            }
+        }
+
+        public Aimbot_iOSDev()
+        {
+            InitializeComponent();
+            update();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (toggle == 1)
+            {
+                serverModuleAddress = getModuleAddress("server.dll");
+                engineModuleAddress = getModuleAddress("engine.dll");
+                started = true;
+                injectButton.Text = "Stop";
+                label3.Text = "Injected successfully";
+            }
+            else
+            {
+                started = false;
+                injectButton.Text = "Start";
+            }
+            toggle *= -1;
+        }
+
+        private void previousButton_Click(object sender, EventArgs e)
+        {
+            if (enemyBaseAddress >= playerBaseAddress + 0x24)
+            {
+                enemyBaseAddress -= 0x24;            
+            }
+        }
+
+        private void nextButton_Click(object sender, EventArgs e)
+        {
+            if (enemyBaseAddress <= playerBaseAddress + 0x24 * 32)
+            {
+                enemyBaseAddress += 0x24;
+            }
+        }
+    }
+}
